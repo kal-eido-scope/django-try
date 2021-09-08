f12d36b14818ba2b976110f22ee2274240a64ee8
jaladhis <1978633500@qq.com>
2021-09-08 12:54:59 +0800

    2021/9/8
    第7章结束，p90页

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,7 +1,10 @@
+from populate_rango import add_page
 from django.urls import re_path
 from rango import views
 urlpatterns = [
     re_path(r'^$',views.index,name = 'index'),
     re_path(r'about/',views.about,name = 'about'),
-    re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category,name='show_category')
+    re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category,name='show_category'),
+    re_path(r'^add_category/$',views.add_category,name='add_category'),
+    re_path(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/',views.add_page,name='add_page')
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,8 @@
+from django.http.request import HttpRequest
 from django.shortcuts import render,HttpResponse
 from django.conf import settings
 from rango.models import Category,Page
+from rango.forms import CategoryForm, PageForm
 def index(request):
     context_dict = {}
     category_list = Category.objects.order_by('-likes')[:5]
@@ -25,4 +27,37 @@ def show_category(request,category_name_slug):
         context_dict['pages'] = None
         context_dict['category'] = None
     return render(request,'rango/category.html',context_dict)
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':    #是HTTP请求吗？
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():         #表单数据有效
+            form.save(commit=True)  #存储
+            return index(request)   #返回首页，也可显示确认消息
+        else:
+            print(form.errors)      #显示错误
+    context_dict = {'form':form}
+    return render(request,'rango/add_category.html',context_dict)
+def add_page(request,category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+            return show_category(request,category_name_slug)
+        else:
+            print(form.errors)
+    context_dict = {'form':form,'category':category}
+    return render(request,'rango/add_page.html',context_dict)
 # Create your views here.
